d6e5f2a19dbf7bd371c064b4d08750211054a8c8
ouledelbakri <yoebakri017@enseirb-matmeca.fr>
2024-10-10 01:37:44 -0700

    Exercice 3: Isolation du conteneur d'API

--- api/index.mjs
+++ api/index.mjs
@@ -4,7 +4,7 @@ const server = createServer();
 
 try {
   //const address = await server.listen({ port: 3000, host: "0.0.0.0" });
-  const address = await server.listen({ port: 80, host: "0.0.0.0" });
+  const address = await server.listen({ port: 4000, host: "0.0.0.0" });
   console.log(`Server started on ${address}`);
 } catch (err) {
   console.log(err);
