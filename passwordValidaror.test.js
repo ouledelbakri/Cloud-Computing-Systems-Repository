8cf696e5b7e2af280e4c80d398707df694ead9c7
Yassine Ouled el bakri <yassineouledelbakri@Yassines-MacBook-Air.local>
2024-11-05 17:03:05 +0100

    Added test to validate the password's has at least one digit

--- passwordValidaror.test.js
+++ passwordValidaror.test.js
@@ -1,7 +1,19 @@
 const isValidPassword = require('./passwordValidatorV1');
 
 test('Password is valid length', () => {
-    const password = "azertyuiop";
+    const password = "a2zertyue";
+    const password2 = "azertyuiop";
     const isValid = isValidPassword(password);
+    const isNotValid = isValidPassword(password2);
+    expect(isValid).toBe(true);
+    expect(isNotValid).toBe(false);
+});
+
+test('Password has at least one digit', () => {
+    const password = "azertyuiop";
+    const password2 = "azertyuiop1";
+    const isNotValid = isValidPassword(password);
+    const isValid = isValidPassword(password2);
+    expect(isNotValid).toBe(false);
     expect(isValid).toBe(true);
 });
--- passwordValidatorV1.js
+++ passwordValidatorV1.js
@@ -1,5 +1,5 @@
 function isValidPassword(password) {
-    return password.length >= 8;
+    return password.length >= 8 && /\d/.test(password);
 }
 
 module.exports =  isValidPassword;
